0f004997c060a37f57147c2c405ab6137aa90ec0
ITShock <lanovenkyi@gmail.com>
2026-03-08 22:46:17 +0100

    Model Registration

--- My_first_app/admin.py
+++ My_first_app/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 
+from My_first_app.models import Task,SubTask, Category
 # Register your models here.
 
+admin.site.register(Task)
+admin.site.register(SubTask)
+admin.site.register(Category)
